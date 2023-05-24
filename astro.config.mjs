4860f88a53a2850c14d9f6f8525cfdae88d129a2
wuluru1002320 <luju.wu@104.com.tw>
2023-05-24 22:33:23 +0800

    feat(lesson5): rss & preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), preact()]
 });
